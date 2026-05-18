ba581f7f1db32761f0766913c18e8dd1df2b836c
くんジェット <130436764+saekacutie@users.noreply.github.com>
2026-05-18 17:18:30 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
-/usr/local/openresty/bin/openresty -g 'daemon off;' &
-sleep 2
-exec /usr/local/bin/xray run -c /etc/xray/config.json
+FROM ghcr.io/xtls/xray-core:latest AS xray-bin
+FROM openresty/openresty:alpine-fat
+...
+COPY --from=xray-bin /usr/bin/xray /usr/local/bin/xray
